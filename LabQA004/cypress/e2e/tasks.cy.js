d4d83b8d9be234f2eb328732ea1e0784c6f26aa6
robsoncerqueira <robsonwebpro@gmail.com>
2024-01-23 18:31:53 -0300

    Tetes Independente

--- LabQA004/cypress/e2e/tasks.cy.js
+++ LabQA004/cypress/e2e/tasks.cy.js
@@ -25,4 +25,40 @@ describe('tarefas', ()=> {
 
     })
 
+    it.only('Não deve permitir tarefa duplicada', () => {
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {
+                name : 'Estudar Javascript'
+            }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: {
+                name : 'Estudar Javascript',
+                is_done: false
+            }
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+
+        cy.visit('http://localhost:8080')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Estudar Javascript')
+
+        cy.contains('button', 'Create').click()
+
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
+
+
+
+    })
 })
