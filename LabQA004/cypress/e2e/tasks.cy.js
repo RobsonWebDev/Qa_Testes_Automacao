018b36f95046f1f0dfd0b5cb9342ef80783a974d
robsoncerqueira <robsonwebpro@gmail.com>
2024-01-23 22:27:03 -0300

    Dividindo em contexto para Organizacao de codigo

--- LabQA004/cypress/e2e/tasks.cy.js
+++ LabQA004/cypress/e2e/tasks.cy.js
@@ -1,44 +1,93 @@
 /// <reference types="cypress" />
 
-describe('tarefas', ()=> {
+describe('tarefas', () => {
+    context('cadastro', () => {
+        it('Deve criar uma nova Terefa', () => {
 
-    it('Deve criar uma nova Terefa', () => {
+            const taskName = 'Ler um Livro de QA'
 
-        const taskName = 'Ler um Livro de QA'
+            cy.removeTaskByName(taskName)
 
-        cy.removeTaskByName(taskName)
-        
-        cy.createTask(taskName)
+            cy.createTask(taskName)
 
-        cy.contains('main div p', taskName)
-            .should('be.visible')
+            cy.contains('main div p', taskName)
+                .should('be.visible')
 
+        })
+
+        it('Não deve permitir tarefa duplicada', () => {
+
+            const task = {
+                name: 'Estudar Javascript',
+                is_done: false
+            }
+
+            cy.removeTaskByName(task.name)
+
+            cy.postTask(task)
+
+            cy.createTask(task.name)
+
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+
+
+
+        })
+
+        it('Campo Obrigatorio', () => {
+            cy.createTask()
+
+            cy.isRequired('This is a required field')
+        })
     })
 
-    it('Não deve permitir tarefa duplicada', () => {
+    context('atualizar', () => {
+        it('Deve concluir uma terefa', () => {
+            const task = {
+                name: 'Estudar Javascript',
+                is_done: false
+            }
 
-        const task = {
-            name : 'Estudar Javascript',
-            is_done: false
-        }
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
 
-        cy.removeTaskByName(task.name)
+            cy.visit('http://localhost:8080')
 
-        cy.postTask(task)
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemToggle]')
+                .click()
 
-        cy.createTask(task.name)
-
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
+            cy.contains('p', task.name)
+                .should('have.css', 'text-decoration-line', 'line-through')
 
 
-
+        })
     })
 
-    it.only('Campo Obrigatorio', ()=> {
-        cy.createTask()
+    context('remover', () => {
+        it('Deve excluir uma terefa', () => {
+            const task = {
+                name: 'Fazer Compras',
+                is_done: false
+            }
 
-        cy.isRequired('This is a required field')
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            cy.visit('http://localhost:8080')
+
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemDelete]')
+                .click()
+
+            cy.contains('p', task.name)
+                .should('not.exist')
+
+
+        })
     })
 })
